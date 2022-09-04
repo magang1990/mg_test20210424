50159a3554dcf2b33da30e41953141485d4b49a9
mg <370769529@qq.com>
2022-09-04 23:00:09 +0800

    优化

---
+++ java_project/src/com/mg/debug/DeBugTest.java
@@ -0,0 +1,20 @@
+package com.mg.debug;
+
+/**
+ * @author mg
+ * @Date 2022-08-29
+ * @Version 1.0
+ */
+public class DeBugTest {
+
+	public static void run(){
+		String s="mg";
+		System.out.println(s);
+	}
+
+
+	public static void main(String[] args){
+	  run();
+	}
+
+}
