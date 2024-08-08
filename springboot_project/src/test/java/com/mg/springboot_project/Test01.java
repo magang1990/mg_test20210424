c44a8199c35cf36810dc97a008a801d7ed23ac54
mg <370769529@qq.com>
2024-08-08 13:59:39 +0800

    guava和Hutool的集合工具根据一定尺寸分割集合

--- springboot_project/src/test/java/com/mg/springboot_project/Test01.java
+++ springboot_project/src/test/java/com/mg/springboot_project/Test01.java
@@ -1,10 +1,15 @@
 package com.mg.springboot_project;
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.google.common.collect.Lists;
 import com.mg.mgdemospringbootstarter.service.MgDemoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mg
@@ -36,4 +41,17 @@ public class Test01 {
     public void run3(){
         System.out.println(address);
     }
+
+    /**
+     * guava和Hutool的集合工具根据一定数量分割集合
+     */
+    @Test
+    public void run4(){
+        //List<Integer> integers = Lists.newArrayList(1, 2, 3, 4, 5);
+        //List<Integer> integers = null;
+        List<Integer> integers = new ArrayList<>();
+        List<List<Integer>> split = CollectionUtil.split(integers, 2);//传参集合为null或空集合时，返回的是空集合
+        List<List<Integer>> partition = Lists.partition(integers, 2);//传参集合为null时，报空指针异常；传参集合为空集合时，返回的是空集合
+    }
+
 }
